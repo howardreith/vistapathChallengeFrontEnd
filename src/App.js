d843bb009af2364369c4867e1511f9e1f9e4d162
howardreith <howard.reith@gmail.com>
2022-09-14 13:08:16 -0400

    get linter working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import LandingPage from "./components/LandingPage/LandingPage";
+import React from 'react';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="appWrapper">
       <LandingPage />
     </div>
   );
--- src/components/LandingPage/LandingPage.test.js
+++ src/components/LandingPage/LandingPage.test.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import LandingPage from "./LandingPage";
+import LandingPage from './LandingPage';
 
 describe('LandingPage', () => {
   function sutFactory() {
-    return render(<LandingPage />)
+    return render(<LandingPage />);
   }
 
   it('should render the landing page', () => {
-    sutFactory()
-    expect(screen.getByTestId('landingPage')).toBeInTheDocument()
-  })
-})
+    sutFactory();
+    expect(screen.getByTestId('landingPage')).toBeInTheDocument();
+  });
+});
